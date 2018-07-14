207918e99338b2d6e98637dfdf07c1835d5ac9e5
Mam Peter <sattarkarashi@ymail.com>
2018-07-14 10:55:26 +0430

    next commit

--- portproject/settings.py
+++ portproject/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blog.apps.BlogConfig',
+    'jobs.apps.JobsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,9 +77,16 @@ WSGI_APPLICATION = 'portproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+
+
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfoliodb',
+        'USER': 'postgres',
+        'PASSWORD': '1122558899a',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
+
 }
 
 
@@ -118,3 +127,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_ROOT=os.path.join(BASE_DIR, 'media')
+MEDIA_URL='/media/'
--- portproject/urls.py
+++ portproject/urls.py
@@ -15,9 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+import blog.views
 
 
 
 urlpatterns = [
+
     path('admin/', admin.site.urls),
-]
+    path('',blog.views.home, name='home'),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
