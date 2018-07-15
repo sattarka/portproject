2b1653a2f56969e26ae5a09df36f7a05f12ec154
Mam Peter <sattarkarashi@ymail.com>
2018-07-15 12:05:33 +0430

    third commit

--- jobs/views.py
+++ jobs/views.py
@@ -1,3 +1,6 @@
+
 from django.shortcuts import render
 
-# Create your views here.
+
+def alljobs(request):
+    return render(request, 'jobs/alljobs.html')
--- portproject/urls.py
+++ portproject/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path,include
 from django.conf import settings
 from django.conf.urls.static import static
 import blog.views
@@ -24,5 +24,12 @@ import blog.views
 urlpatterns = [
 
     path('admin/', admin.site.urls),
+
+
     path('',blog.views.home, name='home'),
+    path('<int:blog_id>/',blog.views.detail, name='detail'),
+
+
+
+    path('jobs', include('jobs.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
